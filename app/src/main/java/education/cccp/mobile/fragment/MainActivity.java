6813fe596e88f92e16d1c502f3133e7374b9d206
mohammedbellahcene <mohammedbellahcene95@gmail.com>
2022-04-12 13:26:45 +0200

    moh et sa branche

--- app/src/main/java/education/cccp/mobile/fragment/MainActivity.java
+++ app/src/main/java/education/cccp/mobile/fragment/MainActivity.java
@@ -1,16 +1,19 @@
 package education.cccp.mobile.fragment;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
 
     }
 }
