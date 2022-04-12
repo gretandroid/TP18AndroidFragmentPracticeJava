a349c75f89458fbffc80a03b9b99347b624563f1
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-04-12 10:09:46 +0200

    création de la classe data

--- app/src/main/java/education/cccp/mobile/fragment/Data.java
+++ app/src/main/java/education/cccp/mobile/fragment/Data.java
@@ -1,6 +1,16 @@
 package education.cccp.mobile.fragment;
 
 public class Data {
-String [] categories={"boissons","gateaux","fruits","légumes"};
-String [][] articles=
+    static String[] categories = {
+            "boissons",
+            "gateaux",
+            "fruits",
+            "légumes"
+    };
+    static String[][] articles = {
+            {"coca-cola", "pepsi", "orangina"},
+            {"pepito", "granola", "makrout"},
+            {"orange", "raisin", "pomme"},
+            {"carrote", "tomate", "oignon"}
+    };
 }
